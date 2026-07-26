3a12f804bacf35410683aa1eec2af1e76facf052
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2026-07-27 09:51:55 +1000

    testing deploy css fuck up

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  base: '/investors',
-  site: 'https://soggyinkgames.com',
+  // base: '/investors',
+  // site: 'https://soggyinkgames.com',
 });
 
